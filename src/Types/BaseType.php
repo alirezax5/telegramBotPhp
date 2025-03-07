36b3222322a9f7ff76aa9842ce0032466a76eb9c
alirezax5 <alirezaaghax6@gmail.com>
2025-03-07 12:48:50 +0330

    fix bug

--- src/Types/BaseType.php
+++ src/Types/BaseType.php
@@ -10,81 +10,47 @@ use JsonSerializable;
  */
 abstract class BaseType implements JsonSerializable
 {
-    /**
-     * داده‌های کلاس که به‌صورت داینامیک تنظیم می‌شوند.
-     * @var array
-     */
     protected array $data;
 
-    /**
-     * مقداردهی اولیه کلاس با داده‌های آپدیت.
-     * @param array $data
-     */
     public function __construct(array $data = [])
     {
-        $this->data = $data;
+        $this->data = $this->mapToObjects($data);
     }
 
-    /**
-     * متد جادویی برای دریافت متغیرهای داینامیک.
-     * اگر مقدار وجود نداشته باشد، مقدار `null` برمی‌گرداند.
-     *
-     * @param string $name نام متغیر
-     * @return mixed|null مقدار متغیر یا null در صورت عدم وجود
-     */
+   
+    protected function mapToObjects(array $data): array
+    {
+        foreach ($data as $key => $value) {
+            if (is_array($value) && $this->isAssocArray($value)) {
+                $data[$key] = new static($value);
+            } elseif (is_array($value)) {
+                $data[$key] = array_map(fn($item) => is_array($item) ? new static($item) : $item, $value);
+            }
+        }
+        return $data;
+    }
+
+
+    protected function isAssocArray(array $array): bool
+    {
+        return array_keys($array) !== range(0, count($array) - 1);
+    }
+
     public function __get(string $name)
     {
         return $this->data[$name] ?? null;
     }
 
-    /**
-     * بررسی می‌کند که مقدار خاصی در داده‌های کلاس وجود دارد یا خیر.
-     *
-     * @param string $key نام کلید
-     * @return bool
-     */
-    public function has(string $key): bool
-    {
-        return array_key_exists($key, $this->data) && !is_null($this->data[$key]);
-    }
-
-    /**
-     * تبدیل داده‌های کلاس به آرایه.
-     *
-     * @return array
-     */
     public function toArray(): array
     {
         return $this->data;
     }
 
-    /**
-     * تبدیل داده‌های کلاس به JSON.
-     *
-     * @return string
-     */
-    public function toJson(): string
-    {
-        return json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-    }
-
-    /**
-     * متد مخصوص `json_encode()` در PHP.
-     * هنگام `json_encode($object)`, مقدار `toArray()` را برمی‌گرداند.
-     *
-     * @return array
-     */
     public function jsonSerialize(): array
     {
         return $this->toArray();
     }
 
-    /**
-     * متد استاتیک برای ایجاد نمونه‌ای از کلاس.
-     *
-     * @param array $data
-     * @return static
-     */
     public static function create(array $data): static
     {
         return new static($data);
